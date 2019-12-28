ed0814a595c0537bdf289da9c8b53fc9742652f6
WheatleyTheCore <alex.colwell@icloud.com>
2019-12-28 17:25:32 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [`gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/articles`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
